2b079abf48117a66c7abbf51631fe2131527e02b
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-23 16:48:42 -0300

    Adds UrlParams for GetUserScoresRequest

---
+++ Requests/UrlParams/Enums/ScoreType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.UrlParams.Enums {
+    public enum ScoreType {
+        BEST, FIRSTS, RECENT
+    }
+}
---
+++ Requests/UrlParams/ScoreTypeUrlParam.cs
@@ -0,0 +1,20 @@
+﻿using osu.API.Requests.Parameters;
+using osu.API.Requests.UrlParams.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Parameters {
+    public class ScoreTypeUrlParam : UrlParam {
+        public string Key { get; private set; }
+
+        public string Value { get; private set; }
+        public bool valueOnly => false;
+        public ScoreTypeUrlParam (ScoreType type) {
+            Key = "scores";
+            Value = type.ToString().ToLowerInvariant();
+        }
+    }
+}
