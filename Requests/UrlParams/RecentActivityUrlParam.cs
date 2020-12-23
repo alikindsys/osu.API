8191d5263c4470cd8be50d8fcda8eb327abb93b3
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-23 19:11:40 -0300

    Adds RecentActivityUrlParam

---
+++ Requests/UrlParams/RecentActivityUrlParam.cs
@@ -0,0 +1,19 @@
+﻿using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.UrlParams {
+    public class RecentActivityUrlParam : UrlParam {
+        public string Key { get; private set; }
+
+        public string Value { get; private set; }
+        public bool valueOnly => true;
+        public RecentActivityUrlParam() {
+            Value = "recent_activity";
+        }
+
+    }
+}
