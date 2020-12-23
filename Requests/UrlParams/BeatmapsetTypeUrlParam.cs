2cbe91a1fe7584815061e074828189e4cae6e1f9
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-23 17:46:44 -0300

    Adds GetUserBeatmapsRequest

---
+++ Requests/UrlParams/BeatmapsetTypeUrlParam.cs
@@ -0,0 +1,25 @@
+﻿using osu.API.Data.Enums;
+using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Parameters {
+    public class BeatmapsetTypeUrlParam : UrlParam {
+        public string Key { get; private set; }
+
+        public string Value { get; private set; }
+
+        public bool valueOnly => false;
+
+        public BeatmapsetTypeUrlParam(UserBeatmapsetStatus type) {
+            Key = "beatmapsets";
+            Value = type switch {
+                UserBeatmapsetStatus.RankedAndApproved => "ranked_and_approved",
+                _ => type.ToString().ToLowerInvariant()
+            };
+        }
+    }
+}
---
+++ Requests/Users/GetUserBeatmapsRequest.cs
@@ -0,0 +1,27 @@
+﻿using osu.API.Data;
+using osu.API.Extensions;
+using osu.API.Requests.Parameters;
+using osu.API.Requests.QueryParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Users {
+    public class GetUserBeatmapsRequest : AuthenticatedGetRequest<List<Beatmapset>> {
+        public GetUserBeatmapsRequest(UserUrlParam user, BeatmapsetTypeUrlParam type, string accessToken) : base(accessToken) {
+            UrlParameters.Add(user);
+            UrlParameters.Add(type);
+        }
+        public GetUserBeatmapsRequest(UserUrlParam user, BeatmapsetTypeUrlParam type, string accessToken,
+                LimitQueryParam limit = null, OffsetQueryParam offset = null
+            ) : base(accessToken) {
+            UrlParameters.Add(user);
+            UrlParameters.Add(type);
+
+            QueryParameters.AddOptional(limit);
+            QueryParameters.AddOptional(offset);
+        }
+    }
+}
