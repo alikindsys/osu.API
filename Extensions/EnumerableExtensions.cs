fb7d2797f018ca9e4936fbd7d50d29d53b107ad0
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-23 16:47:25 -0300

    Adds AddOptional extension method

---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Extensions {
+    public static class EnumerableExtensions {
+        public static void AddOptional<T>(this List<T> list, T value) {
+            if (value == null) return;
+            list.Add(value);
+        }
+    }
+}
