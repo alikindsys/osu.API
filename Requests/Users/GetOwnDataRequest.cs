5b5b7b7ac80f3d07f04d57884950481db7928d34
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 17:36:47 -0300

    Implements GetOwnDataRequest

---
+++ Requests/Users/GetOwnDataRequest.cs
@@ -0,0 +1,25 @@
+﻿using osu.API.Data;
+using osu.API.Data.Enums;
+using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Users {
+    public class GetOwnDataRequest : AuthenticatedGetRequest<User> {
+        public GetOwnDataRequest(string accessToken) : base(accessToken) {
+            BaseUrl += "/me";
+        }
+
+        public GetOwnDataRequest(ModeUrlParam mode,string accessToken) : base(accessToken) {
+            BaseUrl += "/me";
+            UrlParameters.Add(mode);
+        }
+
+        public GetOwnDataRequest(Gamemode mode, string accessToken)
+            : this(new ModeUrlParam(mode), accessToken)
+            { }
+    }
+}
