4d59cbc35c38d58fbbcc98d1f89052c12f0fde3c
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 17:05:11 -0300

    Adds ModdingPost model

---
+++ Data/Modding/ModdingPost.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Data {
+    public class ModdingPost {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+}
