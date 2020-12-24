087c918fb98b6dfaedf45cd1ebb8b81950f7bf8b
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 17:16:34 -0300

    Implements fragment support on BaseRequest

--- Requests/BaseRequest.cs
+++ Requests/BaseRequest.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using osu.API.Requests.Parameters;
+using osu.API.Requests.UrlFragments;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,14 @@ namespace osu.API.Requests {
         internal string BaseUrl = "https://osu.ppy.sh/api/v2";
         public List<QueryParam> QueryParameters { get; set; } = new();
         public List<UrlParam> UrlParameters { get; set; } = new();
+        public List<Fragment> UrlFragments { get; set; } = new();
 
         public Dictionary<string, string> Headers {get;set;} = new();
 
         public string Url => string.Join("/", UrlParameters.Select(x => $"{(x.valueOnly ? $"{x.Value}" : $"{x.Key}/{x.Value}")}"));
+        public string Fragments => string.Join("/", UrlFragments.Select(x => x.Value));
         public string Query => string.Join("&", QueryParameters.Select(x => $"{x.Key}={x.Value}"));
-        public string RequestUrl => $"{BaseUrl}/{Url}{(string.IsNullOrWhiteSpace(Query)?"":$"?{Query}")}";
+        public string RequestUrl => $"{BaseUrl}/{Url}{(string.IsNullOrWhiteSpace(Fragments) ? "" : $"/{Fragments}")}{(string.IsNullOrWhiteSpace(Query)?"":$"?{Query}")}";
 
     }
 }
