0aca779829d50d51a172d934d4835c456a707312
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 00:06:49 -0300

    Add overloads for GetUserBeatmapsRequest

--- Requests/Users/GetUserBeatmapsRequest.cs
+++ Requests/Users/GetUserBeatmapsRequest.cs
@@ -1,4 +1,5 @@
 ﻿using osu.API.Data;
+using osu.API.Data.Enums;
 using osu.API.Extensions;
 using osu.API.Requests.Parameters;
 using osu.API.Requests.QueryParams;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace osu.API.Requests.Users {
     public class GetUserBeatmapsRequest : AuthenticatedGetRequest<List<Beatmapset>> {
+
         public GetUserBeatmapsRequest(UserUrlParam user, BeatmapsetTypeUrlParam type, string accessToken) : base(accessToken) {
             UrlParameters.Add(user);
             UrlParameters.Add(type);
@@ -22,6 +24,18 @@ namespace osu.API.Requests.Users {
 
             QueryParameters.AddOptional(limit);
             QueryParameters.AddOptional(offset);
+        }
+
+        public GetUserBeatmapsRequest(ulong userId, UserBeatmapsetStatus type, string accessToken)
+            : this(new UserUrlParam(userId), new BeatmapsetTypeUrlParam(type), accessToken) {
+        }
+
+        public GetUserBeatmapsRequest(ulong userId, UserBeatmapsetStatus type, string accesToken,
+            int limit = 5, int offset = 0)
+            : this(new UserUrlParam(userId), new BeatmapsetTypeUrlParam(type),accesToken,
+                  new LimitQueryParam(limit), new OffsetQueryParam(offset)
+                  ) {
+
         }
     }
 }
