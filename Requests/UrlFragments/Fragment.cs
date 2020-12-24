20bfa46213c0d15c818dd2b0ff00047d02aa4798
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 17:15:52 -0300

    Add Fragment interface to represent UrlFragments

---
+++ Requests/UrlFragments/Fragment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.UrlFragments {
+    public interface Fragment {
+        public string Value {get;}
+    }
+}
