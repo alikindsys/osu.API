500df1c176c79649c6a8a86f5cd87537a2ecc635
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-24 17:20:17 -0300

    Port RecentActivityUrlParam to a UrlFragment since that better represented the object

---
+++ Requests/UrlFragments/RecentActivityFragment.cs
@@ -0,0 +1,13 @@
+﻿using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.UrlFragments {
+    public class RecentActivityFragment : Fragment {
+        public string Value => "recent_activity";
+
+    }
+}
