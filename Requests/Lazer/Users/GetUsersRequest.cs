918a7fce078eb371ee139aadcb7206d41c25e421
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-25 16:46:32 -0300

    Implements GetUsersRequest

---
+++ Requests/Lazer/Users/GetUsersRequest.cs
@@ -0,0 +1,23 @@
+﻿using osu.API.Data;
+using osu.API.Requests.QueryParams;
+using osu.API.Requests.UrlFragments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Lazer {
+    public class GetUsersRequest : LazerRequest<List<User>> {
+        public GetUsersRequest() {
+            UrlFragments.Add(new UsersFragment());
+        }
+
+        public GetUsersRequest(params IdsQueryParam[] ids) {
+            UrlFragments.Add(new UsersFragment());
+            foreach(var id in ids) {
+                QueryParameters.Add(id);
+            }
+        }
+    }
+}
