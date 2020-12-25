2e53ad8eb8d7e467484e397854724eeccc942c02
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-25 16:27:44 -0300

    Adds LazerRequest abstract class for representing "lazer" requests

---
+++ Requests/Lazer/LazerRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Lazer {
+    /// <summary>
+    /// A Lazer request is a request intended for use by the osu!lazer client which isn't currently avaliable
+    /// for use by the public.
+    /// For that reasoning, they are dummy requests that do nothing.
+    /// </summary>
+    public abstract class LazerRequest<T> : BaseRequest<T> {
+    }
+}
