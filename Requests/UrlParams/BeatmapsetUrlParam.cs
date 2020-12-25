1af84e2471cf9840532e1c59d848e0e4bc7ed4ce
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-25 18:26:30 -0300

    Adds BeatmapsetUrlParam

---
+++ Requests/UrlParams/BeatmapsetUrlParam.cs
@@ -0,0 +1,20 @@
+﻿using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.UrlParams {
+    public class BeatmapsetUrlParam : UrlParam {
+        public string Key => "beatmapsets";
+
+        public string Value { get; private set; }
+
+        public bool valueOnly => false;
+
+        public BeatmapsetUrlParam(ulong beatmapsetId) {
+            Value = beatmapsetId.ToString();
+        }
+    }
+}
