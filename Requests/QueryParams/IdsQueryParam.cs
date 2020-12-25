1e05c764bdff25973ee698e882c8e467ac4ef80c
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-25 16:45:54 -0300

    Adds IdsQueryParam

---
+++ Requests/QueryParams/IdsQueryParam.cs
@@ -0,0 +1,17 @@
+﻿using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.QueryParams {
+    public class IdsQueryParam : QueryParam {
+        public string Key => "ids[]";
+
+        public string Value { get; internal set; }
+        public IdsQueryParam (ulong id) {
+            Value = id.ToString();
+        }
+    }
+}
