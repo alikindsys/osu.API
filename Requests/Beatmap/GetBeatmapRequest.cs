13776b57bf4dc36a8dbd647dfbb469c5d8906a48
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 21:58:04 -0300

    Implements GetBeatmapRequest

---
+++ Requests/Beatmap/GetBeatmapRequest.cs
@@ -0,0 +1,20 @@
+﻿using osu.API.Data;
+using osu.API.Requests.UrlParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Beatmap {
+    public class GetBeatmapRequest : AuthenticatedGetRequest<Data.Beatmap> {
+        public GetBeatmapRequest(BeatmapUrlParam beatmap ,string accessToken) : base(accessToken) {
+            UrlParameters.Add(beatmap);
+        }
+
+        public GetBeatmapRequest(ulong beatmapId, string accessToken)
+            : this(new BeatmapUrlParam(beatmapId), accessToken) {
+
+        }
+    }
+}
