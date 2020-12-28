4bdfd791580fa6a4281459e6fe1bdf7ca39ba8bf
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 22:59:17 -0300

    Adds BeatmapIdQueryParam

---
+++ Requests/QueryParams/BeatmapIdQueryParam.cs
@@ -0,0 +1,18 @@
+﻿using osu.API.Requests.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.QueryParams {
+    public class BeatmapIdQueryParam : QueryParam {
+        public string Key => "beatmap_id";
+
+        public string Value { get; internal set; }
+
+        public BeatmapIdQueryParam(ulong beatmapId) {
+            Value = beatmapId.ToString();
+        }
+    }
+}
