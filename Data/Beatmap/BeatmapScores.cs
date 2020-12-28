8155ec4640615dbb8c04210b4869d8a9344fb3ec
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 22:31:23 -0300

    Adds BeatmapScores data type

---
+++ Data/Beatmap/BeatmapScores.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Data {
+    public class BeatmapScores {
+        [JsonProperty("scores")]
+        public List<Score> Scores { get; set; }
+    }
+}
