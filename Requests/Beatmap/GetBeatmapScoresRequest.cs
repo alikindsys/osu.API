3ccf37179b3546b34b5037778868f97f981da55d
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 22:32:08 -0300

    Implements GetBeatmapScoresRequest

---
+++ Requests/Beatmap/GetBeatmapScoresRequest.cs
@@ -0,0 +1,22 @@
+﻿using osu.API.Data;
+using osu.API.Extensions;
+using osu.API.Requests.QueryParams;
+using osu.API.Requests.UrlFragments;
+using osu.API.Requests.UrlParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Beatmap {
+    public class GetBeatmapScoresRequest : AuthenticatedGetRequest<BeatmapScores> {
+        public GetBeatmapScoresRequest(BeatmapUrlParam beatmap, string accessToken,
+            ModeQueryParam mode = null) : base(accessToken) {
+            UrlParameters.Add(beatmap);
+            UrlFragments.Add(new ScoresFragment());
+
+            QueryParameters.AddOptional(mode);
+        }
+    }
+}
