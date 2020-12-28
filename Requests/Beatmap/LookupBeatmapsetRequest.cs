add1b0b21cbf3679d3dcc79044651cd508ac88a5
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 22:59:35 -0300

    Implements LookupBeatmapsetRequest

---
+++ Requests/Beatmap/LookupBeatmapsetRequest.cs
@@ -0,0 +1,21 @@
+﻿using osu.API.Data;
+using osu.API.Requests.QueryParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Beatmap {
+    public class LookupBeatmapsetRequest : AuthenticatedGetRequest<Beatmapset> {
+        public LookupBeatmapsetRequest(BeatmapIdQueryParam beatmap, string accessToken) : base(accessToken) {
+            BaseUrl += "/beatmapsets/lookup";
+            QueryParameters.Add(beatmap);
+        }
+
+        public LookupBeatmapsetRequest(ulong beatmapId, string accessToken)
+            : this(new BeatmapIdQueryParam(beatmapId), accessToken) {
+
+        }
+    }
+}
