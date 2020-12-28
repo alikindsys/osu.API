6fc3f61254535ce53ac859d8792188a5df9bc107
roridev <t3ctotalmenterandom1@outlook.com>
2020-12-27 22:51:18 -0300

    Implements LookupBeatmapRequest

---
+++ Requests/Beatmap/LookupBeatmapRequest.cs
@@ -0,0 +1,23 @@
+﻿using osu.API.Requests.QueryParams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.API.Requests.Beatmap {
+    public class LookupBeatmapRequest : AuthenticatedGetRequest<Data.Beatmap> {
+        public LookupBeatmapRequest(FilenameQueryParam file, string accessToken) : base(accessToken) {
+            BaseUrl += "/beatmaps/lookup";
+            QueryParameters.Add(file);
+        }
+        public LookupBeatmapRequest(ChecksumQueryParam md5Checksum, string accessToken) : base(accessToken) {
+            BaseUrl += "/beatmaps/lookup";
+            QueryParameters.Add(md5Checksum);
+        }
+        public LookupBeatmapRequest(IdQueryParam id, string accessToken) : base(accessToken) {
+            BaseUrl += "/beatmaps/lookup";
+            QueryParameters.Add(id);
+        }
+    }
+}
